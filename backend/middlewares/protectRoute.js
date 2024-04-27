540fd6400c08c467ccc7dc6432f5473c8d826b9b
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-04-27 12:40:33 +0530

    fixed middleware status codes

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -1,18 +1,23 @@
 import User from "../models/User.model.js";
 import jwt from "jsonwebtoken";
+import StatusCodes from "../utils/statusCodes.js";
 // Checking if user is logged in to do actions
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Unauthorized" });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
     req.user = user;
     next();
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: err.message });
     console.log("Error in ProtectRoute ");
   }
 };
