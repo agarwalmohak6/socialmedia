6db60919fe457aad2f514ec936ec4e8775d88c8c
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-04-30 13:57:24 +0530

    navbar created with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
-const App = () => {
+import { Route, Routes } from "react-router-dom";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
   return (
     <>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Add more routes as needed */}
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
