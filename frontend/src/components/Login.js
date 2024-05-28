0f62152227e94195b9dae3304ed3fae050ec791e
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-05-28 13:36:37 +0530

    fixed login register page images

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -74,7 +74,7 @@ export default function LoginPage() {
           md={7}
           sx={{
             backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
+              "url(https://static.vecteezy.com/system/resources/previews/005/879/539/non_2x/cloud-computing-modern-flat-concept-for-web-banner-design-man-enters-password-and-login-to-access-cloud-storage-for-uploading-and-processing-files-illustration-with-isolated-people-scene-free-vector.jpg)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
