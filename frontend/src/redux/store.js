1347bfaf85421f52369f39b5b90623f2f1d87314
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-05-29 14:31:23 +0530

    removed extra code

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,9 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice";
 import postReducer from "./postSlice";
 
 export default configureStore({
   reducer: {
-    auth: authReducer,
     posts: postReducer,
   },
 });
